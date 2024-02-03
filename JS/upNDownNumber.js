fbe064c72ca60b5c3372642bd133df2808a90f29
DEV-HenriQ <devjosehenrique@gmail.com>
2024-02-03 14:26:34 -0300

    Colocando função de pressionar nos botões - e +

--- JS/upNDownNumber.js
+++ JS/upNDownNumber.js
@@ -1,30 +1,77 @@
-const allPlus = document.querySelectorAll('.adicionar-qtd')
-allPlus.forEach(function(cada)
-{
-   cada.addEventListener('click', plusSpan)
-})
+// Botão de mais
 
-const allMinus = document.querySelectorAll('.diminuir-qtd')
-allMinus.forEach(function(cada)
-{
-   cada.addEventListener('click', minusSpan)
-})
+let pressionado = false;
+let intervalId;
 
-function plusSpan()
-{
-   let numberSpan = Number(event.target.previousElementSibling.innerHTML) + 1
-   event.target.previousElementSibling.innerHTML = numberSpan
+const allPlus = document.querySelectorAll('.adicionar-qtd');
+
+allPlus.forEach(function (cada) {
+   cada.addEventListener('mousedown', function () {
+      pressionado = true;
+      intervalId = setInterval(function () {
+         plusSpan(cada); // passando o objeto 'cada' como argumento
+      }, 150);
+   });
+
+   cada.addEventListener('mouseup', function () {
+      pressionado = false;
+      clearInterval(intervalId);
+   });
+
+   cada.addEventListener('click', function () {
+      plusSpan(cada);
+   });
+});
+
+function plusSpan(element) {
+   let qtd = element.previousElementSibling
+   qtd.innerHTML = Number(qtd.innerHTML) + 1;
 }
 
-function minusSpan()
-{
-   if (Number(event.target.nextElementSibling.innerHTML) == 0)
+// Botão de menos
+
+let pressionadoMinus = false;
+let intervalIdMinus;
+
+const allMinus = document.querySelectorAll('.diminuir-qtd');
+
+allMinus.forEach(function (cada) {
+   cada.addEventListener('mousedown', function () {
+      pressionadoMinus = true;
+      intervalIdMinus = setInterval(function () {
+         minusSpan(cada);
+      }, 150);
+   });
+
+   cada.addEventListener('mouseup', function () {
+      pressionadoMinus = false;
+      clearInterval(intervalIdMinus);
+   });
+
+   cada.addEventListener('touchstart', function () {
+      pressionadoMinus = true;
+      intervalIdMinus = setInterval(function () {
+         minusSpan(cada);
+      }, 150);
+   });
+
+   cada.addEventListener('touchend', function () {
+      pressionadoMinus = false;
+      clearInterval(intervalIdMinus);
+   });
+
+   cada.addEventListener('click', function () {
+      minusSpan(cada);
+   });
+});
+
+function minusSpan(element) {
+   if (pressionadoMinus || event.type === 'click')
    {
-      return
-   }
-   else
-   {
-      let numberSpan = Number(event.target.nextElementSibling.innerHTML) - 1
-      event.target.nextElementSibling.innerHTML = numberSpan
+      if (Number(element.nextElementSibling.innerHTML) > 0)
+      {
+         let qtd = element.nextElementSibling
+         qtd.innerHTML = Number(qtd.innerHTML) - 1;
+      }
    }
 }
