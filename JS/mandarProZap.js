017183c0d9db87f6b9a1c6ecfe7b231485d77e85
DEV-HenriQ <devjosehenrique@gmail.com>
2024-04-03 20:44:30 -0300

    Mudando o número de contato

--- JS/mandarProZap.js
+++ JS/mandarProZap.js
@@ -15,7 +15,7 @@ function prepareToZap()
       const pedidoF = transformingToTextZap(pedido)
       const totalApagarFinal = document.querySelector('.msg-dos-itens').children[2].innerText
 
-      mensagemPraFirma = `https://wa.me/+5581983343846?text=PEDIDO%20CANTINHO%20DO%20LANCHE%20-%20VIA%20SITE%0D%0A%0D%0APedido%20de:%20*${nome}*!%0D%0A%0D%0A--------------------------------%0D%0A%0D%0A${pedidoF}%0D%0A%0D%0AObservações:%20${observacoes}%0D%0A%0D%0A${totalApagarFinal}%0D%0A%0D%0AForma%20de%20pagamento:%20${formaPgmt}%0D%0A%0D%0A--------------------------------%0D%0A%0D%0ADados%20Pessoais:%0D%0ANome:%20${nome}%0D%0AEndereço:%20${endereco}%0D%0APonto%20de%20Referência:%20${referencia}`
+      mensagemPraFirma = `https://wa.me/+558199437226?text=PEDIDO%20CANTINHO%20DO%20LANCHE%20-%20VIA%20SITE%0D%0A%0D%0APedido%20de:%20*${nome}*!%0D%0A%0D%0A--------------------------------%0D%0A%0D%0A${pedidoF}%0D%0A%0D%0AObservações:%20${observacoes}%0D%0A%0D%0A${totalApagarFinal}%0D%0A%0D%0AForma%20de%20pagamento:%20${formaPgmt}%0D%0A%0D%0A--------------------------------%0D%0A%0D%0ADados%20Pessoais:%0D%0ANome:%20${nome}%0D%0AEndereço:%20${endereco}%0D%0APonto%20de%20Referência:%20${referencia}`
 
       meuLink.href = mensagemPraFirma
    }, 500)
